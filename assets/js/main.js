01bf6ab493ccffa56645cb1c76f25cb0045aa2c7
rogerio levy <rogerio.levy@live.fr>
2023-09-23 23:09:26 -0400

    utilizando map para resumir e otimizar o codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
-
-const offset = 0
-const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
-
+// const offset = 0;
+// const limit = 10;
+// const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -23,14 +21,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for( let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
- 
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+})
